fdb1dc01b8ad1104ebd1413ca27edea7bb236caa
DmitriyALEX <doiroffical@gmail.com>
2025-10-30 20:42:33 +0200

    fix: added user marker

--- src/components/MapLeaflet/index.tsx
+++ src/components/MapLeaflet/index.tsx
@@ -5,12 +5,29 @@ import MarkerClusterGroup from "react-leaflet-cluster";
 import "./style.css";
 import type { IFetchedUser } from "../../types/fetchedUsers.interface";
 import PopupMap from "../PopupMap";
+//icons
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+const DefaultIcon = L.icon({
+  iconUrl: markerIcon,       
+  iconRetinaUrl: markerIcon2x, 
+  shadowUrl: markerShadow,     
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 type Props = {
   filteredUsers: IFetchedUser[]
 }
 
 const MapLeaflet:React.FC<Props> = ({filteredUsers}) => {
+  
   return (
     <div>
       <MapContainer center={[20, 0]} zoom={2} style={{ height: "100vh", width: "100%" }}>
@@ -33,6 +50,7 @@ const MapLeaflet:React.FC<Props> = ({filteredUsers}) => {
             <Marker
               key={user.id}
               position={[user.location.lat, user.location.lon]}
+              icon={DefaultIcon}
             >
               <Popup>
                 <PopupMap user={user}/>
